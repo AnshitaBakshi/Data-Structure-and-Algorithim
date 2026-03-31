71707e6cb0f84099d0d2ec3d4299e1f6e0680645
AnshitaBakshi <147611604+AnshitaBakshi@users.noreply.github.com>
2026-03-31 23:48:50 +0530

    Add MaxconecOnes class with findmaxConsecones method
    Implement a method to find the maximum consecutive ones in an array.

--- Array/MaxconecOnes.java
+++ Array/MaxconecOnes.java
@@ -1,1 +1,29 @@
 
+public class MaxconecOnes {
+    public int findmaxConsecones(int nums[]){
+        int cnt=0;
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                cnt++;
+            }
+            else{
+                cnt=0;
+
+            }
+            max=Math.max(max, cnt);
+            }
+return max;
+    
+        }
+        
+
+    public static void main(String[] args) {
+        int nums[]={1,1,0,1,1,1};
+        MaxconecOnes mco=new MaxconecOnes();
+        int result=mco.findmaxConsecones(nums);
+        System.out.println(result);
+    }
+    
+}
+
